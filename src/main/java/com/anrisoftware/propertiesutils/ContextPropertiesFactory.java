65cdeb88da7885b923933c7f6971a6e995d9d6e7
Erwin Mueller <erwin.mueller@deventm.org>
2012-07-21 19:42:42 +0200

    Add load properties from resource location
    Finish #1430

--- src/main/java/com/anrisoftware/propertiesutils/ContextPropertiesFactory.java
+++ src/main/java/com/anrisoftware/propertiesutils/ContextPropertiesFactory.java
@@ -2,9 +2,12 @@ package com.anrisoftware.propertiesutils;
 
 import static com.google.common.io.Resources.getResource;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URI;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.Properties;
@@ -21,6 +24,8 @@ public class ContextPropertiesFactory {
 
 	private final String context;
 
+	private final Properties defaultProperties;
+
 	/**
 	 * Sets the specified context object.
 	 * 
@@ -28,7 +33,22 @@ public class ContextPropertiesFactory {
 	 *            the context {@link Object}.
 	 */
 	public ContextPropertiesFactory(Object context) {
-		this(context.getClass());
+		this(context.getClass(), null);
+	}
+
+	/**
+	 * Sets the specified context object.
+	 * 
+	 * @param context
+	 *            the context {@link Object}.
+	 * 
+	 * @param defaultProperties
+	 *            the default {@link Properties}.
+	 * 
+	 * @since 1.1
+	 */
+	public ContextPropertiesFactory(Object context, Properties defaultProperties) {
+		this(context.getClass(), defaultProperties);
 	}
 
 	/**
@@ -38,7 +58,23 @@ public class ContextPropertiesFactory {
 	 *            the context {@link Class}.
 	 */
 	public ContextPropertiesFactory(Class<?> context) {
-		this(context.getPackage().getName());
+		this(context.getPackage().getName(), null);
+	}
+
+	/**
+	 * Sets the specified context class.
+	 * 
+	 * @param context
+	 *            the context {@link Class}.
+	 * 
+	 * @param defaultProperties
+	 *            the default {@link Properties}.
+	 * 
+	 * @since 1.1
+	 */
+	public ContextPropertiesFactory(Class<?> context,
+			Properties defaultProperties) {
+		this(context.getPackage().getName(), defaultProperties);
 	}
 
 	/**
@@ -48,7 +84,34 @@ public class ContextPropertiesFactory {
 	 *            the context.
 	 */
 	public ContextPropertiesFactory(String context) {
+		this(context, null);
+	}
+
+	/**
+	 * Sets the specified context.
+	 * 
+	 * @param context
+	 *            the context.
+	 * 
+	 * @param defaultProperties
+	 *            the default {@link Properties}.
+	 * 
+	 * @since 1.1
+	 */
+	public ContextPropertiesFactory(String context, Properties defaultProperties) {
 		this.context = context;
+		this.defaultProperties = defaultProperties;
+	}
+
+	/**
+	 * Returns the system properties.
+	 * 
+	 * @return the {@link ContextProperties}.
+	 * 
+	 * @since 1.1
+	 */
+	public ContextProperties getSystemProperties() {
+		return new ContextProperties(context, System.getProperties());
 	}
 
 	/**
@@ -148,8 +211,84 @@ public class ContextPropertiesFactory {
 	 */
 	public ContextProperties fromResource(URL resource, Charset charset)
 			throws IOException {
-		Properties properties = new Properties();
+		Properties properties = new Properties(defaultProperties);
 		Reader reader = new InputStreamReader(resource.openStream(), charset);
+		properties.load(reader);
+		return new ContextProperties(context, properties);
+	}
+
+	/**
+	 * Loads the properties from a resource with a default character set.
+	 * 
+	 * @param resource
+	 *            the resource {@link URI}.
+	 * 
+	 * @return the {@link ContextProperties}.
+	 * 
+	 * @throws IOException
+	 *             if there was an error loading the resource.
+	 */
+	public ContextProperties fromResource(URI resource) throws IOException {
+		return fromResource(resource, DEFAULT_CHARSET);
+	}
+
+	/**
+	 * Loads the properties from a resource with a specified character set.
+	 * 
+	 * @param resource
+	 *            the resource {@link URI}.
+	 * 
+	 * @param charset
+	 *            the {@link Charset} of the resource.
+	 * 
+	 * @return the {@link ContextProperties}.
+	 * 
+	 * @throws IOException
+	 *             if there was an error loading the resource.
+	 * 
+	 * @since 1.1
+	 */
+	public ContextProperties fromResource(URI resource, Charset charset)
+			throws IOException {
+		return fromResource(new File(resource), charset);
+	}
+
+	/**
+	 * Loads the properties from a resource with a default character set.
+	 * 
+	 * @param resource
+	 *            the resource {@link File}.
+	 * 
+	 * @return the {@link ContextProperties}.
+	 * 
+	 * @throws IOException
+	 *             if there was an error loading the resource.
+	 */
+	public ContextProperties fromResource(File resource) throws IOException {
+		return fromResource(resource, DEFAULT_CHARSET);
+	}
+
+	/**
+	 * Loads the properties from a resource with a specified character set.
+	 * 
+	 * @param resource
+	 *            the resource {@link File}.
+	 * 
+	 * @param charset
+	 *            the {@link Charset} of the resource.
+	 * 
+	 * @return the {@link ContextProperties}.
+	 * 
+	 * @throws IOException
+	 *             if there was an error loading the resource.
+	 * 
+	 * @since 1.1
+	 */
+	public ContextProperties fromResource(File resource, Charset charset)
+			throws IOException {
+		Properties properties = new Properties(defaultProperties);
+		Reader reader = new InputStreamReader(new FileInputStream(resource),
+				charset);
 		properties.load(reader);
 		return new ContextProperties(context, properties);
 	}
