d9026f66fd19ddd9e69f4ecfd5569a09ddb37b51
Erwin Mueller <erwin.mueller@deventm.org>
2012-10-03 22:30:18 +0200

    Add license headers
    #1642

--- propertiesutils-swingproperties-groovy/src/main/java/com/anrisoftware/propertiesutils/GroovyEvaluating.java
+++ propertiesutils-swingproperties-groovy/src/main/java/com/anrisoftware/propertiesutils/GroovyEvaluating.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of propertiesutils-swingproperties.
+ * This file is part of propertiesutils-swingproperties-groovy.
  *
- * propertiesutils-swingproperties is free software: you can redistribute it and/or modify it
+ * propertiesutils-swingproperties-groovy is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * propertiesutils-swingproperties is distributed in the hope that it will be useful, but
+ * propertiesutils-swingproperties-groovy is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with propertiesutils-swingproperties. If not, see <http://www.gnu.org/licenses/>.
+ * along with propertiesutils-swingproperties-groovy. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.propertiesutils;
 
