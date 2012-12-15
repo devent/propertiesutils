c5e06ce9e1c6da5ee19462ed210f5d637f8d8bd5
Erwin Mueller <erwin.mueller@deventm.org>
2012-12-15 12:47:14 +0100

    Add duration and period properties

--- propertiesutils-contextproperties/src/main/java/com/anrisoftware/propertiesutils/ContextProperties.java
+++ propertiesutils-contextproperties/src/main/java/com/anrisoftware/propertiesutils/ContextProperties.java
@@ -45,6 +45,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.joda.time.Duration;
+import org.joda.time.Period;
+import org.joda.time.format.ISOPeriodFormat;
+import org.joda.time.format.PeriodFormatter;
+
 /**
  * <p>
  * Properties with a specified context.
@@ -715,6 +720,76 @@ public class ContextProperties extends Properties {
 			String separatorChars) {
 		String property = getProperty(key, join(defaultValue, ","));
 		return Arrays.asList(split(property, separatorChars));
+	}
+
+	/**
+	 * Returns a time period property using the format defined in
+	 * {@link ISOPeriodFormat#standard()}.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @return the {@link Period}.
+	 * 
+	 * @since 1.5
+	 */
+	public Period getPeriodProperty(String key) {
+		String property = getProperty(key);
+		return new Period(property);
+	}
+
+	/**
+	 * Returns a time period property using the format defined in
+	 * {@link ISOPeriodFormat#standard()}.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @param formatter
+	 *            the {@link PeriodFormatter} that parses the period property.
+	 * 
+	 * @return the {@link Period}.
+	 * 
+	 * @since 1.5
+	 */
+	public Period getPeriodProperty(String key, PeriodFormatter formatter) {
+		String property = getProperty(key);
+		return formatter.parsePeriod(property);
+	}
+
+	/**
+	 * Returns a time duration property using the format defined in
+	 * {@link ISOPeriodFormat#standard()}.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @return the {@link Duration}.
+	 * 
+	 * @since 1.5
+	 */
+	public Duration getDurationProperty(String key) {
+		String property = getProperty(key);
+		return new Period(property).toStandardDuration();
+	}
+
+	/**
+	 * Returns a time duration property using the format defined in
+	 * {@link ISODurationFormat#standard()}.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @param formatter
+	 *            the {@link PeriodFormatter} that parses the duration property.
+	 * 
+	 * @return the {@link Duration}.
+	 * 
+	 * @since 1.5
+	 */
+	public Duration getDurationProperty(String key, PeriodFormatter formatter) {
+		String property = getProperty(key);
+		return formatter.parsePeriod(property).toStandardDuration();
 	}
 
 	@Override
