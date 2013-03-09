8c7453822fe4db87d0f3e291b7fbc566347333e8
Erwin Mueller <erwin.mueller@deventm.org>
2013-03-09 13:53:21 +0100

    Add AWT context properties
    * Color
    * Point2D
    
    #2239

---
+++ propertiesutils-contextproperties/src/main/java/com/anrisoftware/propertiesutils/AwtContextProperties.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of propertiesutils-contextproperties.
+ *
+ * propertiesutils-contextproperties is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * propertiesutils-contextproperties is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with propertiesutils-contextproperties. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.propertiesutils;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.text.ParseException;
+import java.util.Properties;
+
+import com.anrisoftware.globalpom.format.point.PointFormat;
+
+/**
+ * Properties with a specified context returning AWT properties.
+ * 
+ * <ul>
+ * <li>{@link Color}</li>
+ * <li>{@link Point2D}</li>
+ * </ul>
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.6
+ */
+@SuppressWarnings("serial")
+public class AwtContextProperties extends ContextProperties {
+
+	/**
+	 * Sets the context and the properties.
+	 * 
+	 * @param context
+	 *            an {@link Object} that is used as the context.
+	 * 
+	 * @param parentProperties
+	 *            the {@link Properties} that are returned.
+	 */
+	public AwtContextProperties(Class<?> context, Properties parentProperties) {
+		super(context, parentProperties);
+	}
+
+	/**
+	 * Sets the context and the properties.
+	 * 
+	 * @param context
+	 *            an {@link Object} that is used as the context.
+	 * 
+	 * @param parentProperties
+	 *            the {@link Properties} that are returned.
+	 */
+	public AwtContextProperties(Object context, Properties parentProperties) {
+		super(context, parentProperties);
+	}
+
+	/**
+	 * Sets the context and the properties.
+	 * 
+	 * @param context
+	 *            the context.
+	 * 
+	 * @param parentProperties
+	 *            the {@link Properties} that are returned.
+	 */
+	public AwtContextProperties(String context, Properties parentProperties) {
+		super(context, parentProperties);
+	}
+
+	/**
+	 * Returns a color property using the format defined in
+	 * {@link Color#decode(String)}.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @return the {@link Color} from the property or {@code null} if no
+	 *         property with the key was found.
+	 */
+	public Color getColorProperty(String key) {
+		String property = getProperty(key);
+		return Color.decode(property);
+	}
+
+	/**
+	 * Returns a color property using the format defined in
+	 * {@link Color#decode(String)}.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @param defaultValue
+	 *            the default {@link Color}.
+	 * 
+	 * @return the {@link Color} from the property or the default color if no
+	 *         property with the key was found.
+	 */
+	public Color getColorProperty(String key, Color defaultValue) {
+		String property = getProperty(key);
+		return property == null ? null : Color.decode(property);
+	}
+
+	/**
+	 * Returns a point property using the format: {@code (x, y)}. The
+	 * parenthesis are optional.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @return the {@link Point2D} from the property or {@code null} if no
+	 *         property with the key was found.
+	 * 
+	 * @throws ParseException
+	 *             if the property cannot be parsed to a point.
+	 */
+	public Point2D getPointProperty(String key) throws ParseException {
+		return getTypedProperty(key, new PointFormat());
+	}
+
+	/**
+	 * Returns a point property using the format: {@code (x, y)}. The
+	 * parenthesis are optional.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @param point
+	 *            the {@link Point2D} that should be parsed. If {@code null} a
+	 *            {@link Point2D#Double} is used.
+	 * 
+	 * @return the {@link Point2D} from the property or {@code null} if no
+	 *         property with the key was found.
+	 * 
+	 * @throws ParseException
+	 *             if the property cannot be parsed to a point.
+	 */
+	public Point2D getPointProperty(String key, Point2D point)
+			throws ParseException {
+		String property = getProperty(key);
+		return new PointFormat().parse(property, point);
+	}
+
+	/**
+	 * Returns a point property using the format: {@code (x, y)}. The
+	 * parenthesis are optional.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * 
+	 * @param defaultValue
+	 *            the default {@link Point2D}.
+	 * 
+	 * @throws ParseException
+	 *             if the property cannot be parsed to a point.
+	 * 
+	 * @return the {@link Point2D} from the property or the default point if no
+	 *         property with the key was found.
+	 */
+	public Point2D getPointPropertyWithDefault(String key, Point2D defaultValue)
+			throws ParseException {
+		return getTypedProperty(key, new PointFormat(), defaultValue);
+	}
+
+}
